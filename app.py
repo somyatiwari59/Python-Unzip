299c50a1280a205569702c86279f5fd814f6b1a8
Somya Tiwari <somyatiwari59@gmail.com>
2020-05-16 14:01:33 +0530

    final

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
 import flask
 from flask import request
+from flask import jsonify
 import zipfile
 import requests
 from io import BytesIO
-import base64
+import json
+
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['POST'])
 def main():
     myZipFile = request.data
-    print(myZipFile)
     headers = flask.request.headers
+    respData = {}
     zipdata = BytesIO()
     zipdata.write(myZipFile)
     pID = headers.get('parent-id')
     print(pID)
-    while( zipfile.is_zipfile(zipdata) == False):
-        myZipFile = request.data
-        zipdata = BytesIO()
-        zipdata.write(myZipFile)
+    i = 1
     with zipfile.ZipFile(zipdata) as zip_ref:
         for info in zip_ref.infolist():
             data = info.filename
             myHTML = zip_ref.read(data)
             headToSend = {'parent-id' : pID}
+            respData['fileName' + i] = data
             req = requests.post('https://ccdev3-moneyspot.cs57.force.com/services/apexrest/GetSeperateFiles/', data=myHTML, headers=headToSend)
-            print(req)
-    return 'Success'
+            respData['fileName' + i + 'status'] = req
+            i = i + 1
+    response = app.response_class(
+        response = json.dumps(respData),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
